2e83df217c24f4d9860776c2a11c14c7cfb76041
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-10 04:57:44 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+var express = require('express')
+const cricLive = require('cric-live');
+/* Requiring body-parser package
+to fetch the data that is entered
+by the user in the HTML form.*/
+const bodyParser = require('body-parser')
+// Allowing app to use body parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+const url = 'http://localhost:3000'
+const krl = 'https://stackoverflowclonefrontend.netlify.app'
+app.use(cors({ origin: krl, credentials: true }))
+
+
+const uri ='mongodb+srv://rajeshmn47:uni1ver%40se@cluster0.bpxam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.Promise = global.Promise
+mongoose.connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (error) {
+    if (error) {
+      console.log('Error!' + error)
+    }
+  }
+)
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+  console.warn(`App listening on http://localhost:${PORT}`)
+})
