eaeb82f535392c2db05d6abb27516d0edce2cfda
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-13 07:43:41 +0530

    make it better

--- controllers/addlivedetail.js
+++ controllers/addlivedetail.js
@@ -15,12 +15,14 @@ function compare(a, b){
 }
 
 module.exports.addLivematchtodb = async function(){
-   const matches=await Match.find()
+   const matches=await Match.find({"match_date": {
+    $gte: Date(date),
+    $lt: Date(endDate)}})
    for(let i=0;i<matches.length;i++){
        let matchdata=matches[i]
        let mat = await MatchLive.findOne({matchId : matchdata.matchId});
        if(mat){
-
+console.log('matchalreadyexists')
        }
        else{
            var livematch=new MatchLive()
