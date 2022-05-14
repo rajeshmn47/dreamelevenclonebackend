976b607e50045bc9b40d23a02c6468ffc8ee02f4
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-14 19:57:56 +0530

    make it better

--- controllers/addlivescores.js
+++ controllers/addlivescores.js
@@ -16,5 +16,36 @@ module.exports.addLivescorestodb = async function(){
     const matches=await MatchLive.find()
     for(let i=0;i<matches.length;i++){
 console.log(matches[i].matchId)
+const options = {
+    method: 'GET',
+    url: `https://cricket-live-data.p.rapidapi.com/match/${matches[i].matchId}`,
+    headers: {
+    'x-rapidapi-host': 'cricket-live-data.p.rapidapi.com',
+    'x-rapidapi-key': '773ece5d2bmsh8af64b6b53baed6p1e86c9jsnd416b0e51110',
+    useQueryString: true
     }
+}
+let promise = new Promise((resolve,reject) =>{
+    console.log(matches[i].date)
+    if((matches[i].date - date)/(60 * 1000) <= 30){
+        request(options,function(error,response,body){
+            if (error){
+                reject(error);
+            }
+            let s = JSON.parse(body);
+            console.log(s,'kuthhe')
+            resolve(s);
+        })
+        
+    }else{
+        reject('Lineups not out before 30 minutes...');
+    }
+})
+promise.then(async (s)=>{
+console.log(s)
+})
+.catch((error)=>{
+    console.log(error)
+})
+}
 }
