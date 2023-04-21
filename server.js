e3bfb5636543fbe83dc9d3f79512972b42b07fab
rajeshmn47 <rajeshmn47@gmail.com>
2023-04-21 18:55:27 +0530

    removed addmatchids

--- server.js
+++ server.js
@@ -76,7 +76,6 @@ cron.schedule(
   true,
   "America/Los_Angeles"
 );
-addIds.addMatchIds();
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.warn(`App listening on http://localhost:${PORT}`);
