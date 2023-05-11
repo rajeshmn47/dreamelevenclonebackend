9e2f14cd7ef749e12980c2d2cc96c2834afac02e
rajeshmn47 <rajeshmn47@gmail.com>
2023-05-11 20:35:55 +0530

    fixing api key

--- apikeys.js
+++ apikeys.js
@@ -14,9 +14,9 @@ module.exports.getkeys = async function () {
   console.log(keyi, "index");
   let keys = [
     "006ab906e4msha11eadbec0202a7p17e626jsnd019becb8cdc",
+    "77cac70752msh1ce13ec8cd5c240p1160fbjsn5e68d56cf5a5",
     "4c7d3711a8mshb541cbf54879207p15aac7jsn48daf03743bb",
     "22e5eb9581msh463d68b77f60aedp15ca87jsn7178d984f2fc",
-    "77cac70752msh1ce13ec8cd5c240p1160fbjsn5e68d56cf5a5",
     "7d69e855b3msh2af85cba1aea4aep1eefd4jsn98b669d3d010",
     "a5da117d90msh3e694894d3b7dbfp12cc3bjsn8167b3fc201c",
     "5658c3593amshd74d634f219e8d1p148490jsna5981aed9d41",
