5922ee635815a63c8867636804ca7d0f0269f456
rajeshmn47 <rajeshmn47@gmail.com>
2023-05-14 19:35:16 +0530

    fixing keys

--- crickeys.js
+++ crickeys.js
@@ -18,7 +18,6 @@ module.exports.getkeys = async function () {
     "29c032b76emsh6616803b28338c2p19f6c1jsn8c7ad47ac806",
     "3ddef92f6emsh8301b1a8e1fd478p15bb8bjsnd0bb5446cadc",
     "36bfaeaf2bmshf3d4c6a7d578422p138403jsn352bedc63511",
-    "7d69e855b3msh2af85cba1aea4aep1eefd4jsn98b669d3d010",
     "3827482ab0msh2682459121bc4e9p182f86jsn5e5bf239f56d",
     "006ab906e4msha11eadbec0202a7p17e626jsnd019becb8cdc",
     "77cac70752msh1ce13ec8cd5c240p1160fbjsn5e68d56cf5a5",
@@ -26,6 +25,7 @@ module.exports.getkeys = async function () {
     "22e5eb9581msh463d68b77f60aedp15ca87jsn7178d984f2fc",
     "a5da117d90msh3e694894d3b7dbfp12cc3bjsn8167b3fc201c",
     "5658c3593amshd74d634f219e8d1p148490jsna5981aed9d41",
+    "7d69e855b3msh2af85cba1aea4aep1eefd4jsn98b669d3d010",
   ];
   return keys[keyi];
 };
