e6772a4762feeda0335875038cc06e9e3812377f
rajeshmn47 <rajeshmn47@gmail.com>
2024-01-24 18:10:37 +0530

    added live razorpay

--- controllers/payment.js
+++ controllers/payment.js
@@ -18,8 +18,8 @@ const User = require("../models/user");
 dotenv.config();
 
 const instance = new Razorpay({
-  key_id: "rzp_test_3FLuLisPuowtZP",
-  key_secret: "paGWw3r0v1ty8K3U9YDxOu8f",
+  key_id: "rzp_live_kGI6IELE9BgRsa",
+  key_secret: "n9WTIxxz5uH3ciJR61jrpQ5s",
 });
 
 router.get("/createpayment/:amount", (req, res) => {
