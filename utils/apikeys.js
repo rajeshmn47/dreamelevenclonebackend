70f5f77130f7e7bcf44e0194fa09ac149ec5692b
rajeshmn47 <rajeshmn47@gmail.com>
2025-09-10 08:32:27 +0530

    updating apikeys

--- utils/apikeys.js
+++ utils/apikeys.js
@@ -1,6 +1,6 @@
 module.exports.squadkeys = async function () {
   const keys=process.env.squadkeys.replace(/(\r\n|\n|\r)/gm, "").replace(/ /g, "")
   .split(",");
-  const keyi = Math.floor(Math.floor(new Date().getHours())/5);
+  const keyi = Math.floor(Math.floor(new Date().getHours())/4);
   return keys[keyi];
 };
