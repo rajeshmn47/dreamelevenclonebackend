5922558551d134016e405d765e38d78517e0f939
rajeshmn47 <rajeshmn47@gmail.com>
2025-09-14 07:28:09 +0530

    fixing keys

--- utils/crickeys.js
+++ utils/crickeys.js
@@ -5,10 +5,12 @@ const User = require("../models/user");
 
 module.exports.getkeys = async function (matchId) {
   //console.log(keys, keyi, 'keyies')
-  const key = await RapidApiKey.findOne({ type: 'lineups', status: 'active' })
+  const key = await RapidApiKey.findOne({ type: 'scores', status: 'active' })
+  if(key){
   await ApiRequest.create({
     matchId: matchId,     // Cricbuzz matchId
     apiKey: key.apiKey
   })
-  return key.apiKey
+}
+  return key?.apiKey
 };
